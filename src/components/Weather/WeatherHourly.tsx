47c6b1e2b4df0ab74c103cd86f179b1006dae249
Glauber Brack <glauber@brack.com.br>
2021-03-30 22:35:57 -0300

    feat: weather hourly component

---
+++ src/components/Weather/WeatherHourly.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { Column, Text } from '~/components';
+
+const WeatherHourlyComponent: React.FC = () => {
+  return (
+    <Column
+      p={10}
+      backgroundColor='blue'
+      alignItems='center'
+      justifyContent='space-between'
+      width={100}
+      borderRadius={4}
+    >
+      <Text>22:00</Text>
+      <Text>imagem</Text>
+      <Text>ceu sujo</Text>
+      <Text>graus</Text>
+    </Column>
+  );
+};
+
+export default WeatherHourlyComponent;
--- src/components/Weather/index.ts
+++ src/components/Weather/index.ts
@@ -1,1 +1,2 @@
 export { default as Weather } from './Weather';
+export { default as WeatherHourly } from './WeatherHourly';
