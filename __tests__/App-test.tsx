07b088d4cfc53c9ef9e634aa544feebbe5b8297d
Glauber Brack <glauber@brack.com.br>
2021-03-31 08:27:07 -0300

    feat: add some components tests

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Text } from 'react-native';
 import renderer from 'react-test-renderer';
 
-import { Column, Row, SingleItemCard } from '../src/components';
+import { Column, Row } from '../src/components';
 
 // Note: test renderer must be required after react-native.
 describe('Components rendering', () => {
@@ -12,13 +12,5 @@ describe('Components rendering', () => {
 
   it('should render Row component', () => {
     renderer.create(<Row />);
-  });
-
-  it('should render SingleItemCard component', () => {
-    renderer.create(
-      <SingleItemCard>
-        <Text>Single card </Text>
-      </SingleItemCard>
-    );
   });
 });
