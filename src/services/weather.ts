2e8fcadbbb01535db5462d95ccc4fbd03523cc5f
Glauber Brack <glauber@brack.com.br>
2021-03-31 00:32:55 -0300

    refactor: services

--- src/services/weather.ts
+++ src/services/weather.ts
@@ -1,8 +1,8 @@
 import { API_KEY } from '~/constants/api-key';
 import fetchClient from '~/providers/fetchClient';
 
-export const getWeather = (lat: number, long: number): Promise<void> =>
+export const getWeather = (lat: number, long: number): Promise<any> =>
   fetchClient.get(`/weather?lat=${lat}&lon=${long}&appid=${API_KEY}`);
 
-export const getWeatherHoruly = (lat: number, long: number): Promise<void> =>
+export const getWeatherHourly = (lat: number, long: number): Promise<any> =>
   fetchClient.get(`/onecall?lat=${lat}&lon=${long}&appid=${API_KEY}`);
